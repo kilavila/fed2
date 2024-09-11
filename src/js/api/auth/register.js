0ee1a5dfdceab966229c93ad0c4e5fd0b24e2613
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-09-11 19:28:59 +0200

    fix regex auth, register start api and js.

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,3 +1,11 @@
+import { headers } from "../headers";
+import {
+  API_KEY,
+  API_AUTH_LOGIN,
+  API_AUTH_REGISTER,
+  API_SOCIAL_POSTS,
+} from "./../../api/constants";
+
 export async function register({
   name,
   email,
@@ -5,4 +13,23 @@ export async function register({
   bio,
   banner,
   avatar,
-}) {}
+}) {
+  try{
+    const reqBody = { name, email, password, bio, banner, avatar };
+    
+    const response = await fetch(API_AUTH_REGISTER, {
+      method: "POST",
+      headers: headers(),
+      body: JSON.stringify(reqBody),
+    });
+
+    if (!response.ok) { 
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    return result.data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,52 @@
-export async function onRegister(event) {}
+import { register } from "../../api/auth/register";
+
+export async function onRegister(event) {
+  event.preventDefault();
+
+  const form = event.target;
+
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  const nameRegex = /^[a-zA-Z\s]*$/;
+  const emailRegex = /[^@]*@stud\.noroff\.no/g;
+  const pswRegex = /[a-zA-Z0-9]{8,20}/g;
+
+  if (!nameRegex.test(name)) {
+    alert("Navn ikke gylding, må være et ord");
+    return;
+  }
+  if (!emailRegex.test(email)) {
+    alert("Epost ikke gylding, må være en stud.noroff.no epost");
+    return;
+  }
+  if (!pswRegex.test(password)) {
+    alert("Passord ikke gylding, må være minst 8 tegn");
+    return;
+  }
+
+  if (!name || !email || !password) {
+    alert("Alle felter er obligatoriske");
+    return;
+  }
+
+  const formData = {
+    name,
+    email,
+    password,
+  };
+
+  try {
+    // Kall register-funksjonen (husk å implementere den riktig)
+    const result = await register(formData);
+
+    alert("Registration successful!");
+    console.log("Registration data:", result);
+
+    // window.location.href = '/welcome';
+  } catch (error) {
+    // Håndter feil (f.eks. vise en feilmelding)
+    console.error("Registration failed:", error);
+    alert("Registration failed. Please try again.");
+  }
+}
