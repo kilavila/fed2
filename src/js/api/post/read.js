11515b10961a0615a4dbe6a6dc352e57e2bb1b8b
EPetrineLynghaug <136695793+EPetrineLynghaug@users.noreply.github.com>
2024-09-20 21:44:33 +0200

    api,home js, index.js firstpage started

--- src/js/api/post/read.js
+++ src/js/api/post/read.js
@@ -17,6 +17,25 @@ export async function readPost(id) {
 }
 
 //get posts- bare forskjelldige måter å hente ting på//
-export async function readPosts(limit = 12, page = 1, tag) {}
+export async function readPosts(limit = 12, page = 1, tag) {
+  try {
+    const response = await fetch(
+      `${API_SOCIAL_POSTS}?limit=${limit}&page=${page}&tag=${tag}`,
+      {
+        method: "GET",
+        headers: headers(),
+      }
+    );
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Response Status: ${response.status}`);
+    }
+    const result = await response.json();
+    console.log(result);
+    return result.data;
+  } catch (error) {
+    console.error("Error reading posts:", error);
+  }
+}
 
 export async function readPostsByUser(username, limit = 12, page = 1, tag) {}
--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,14 +1,53 @@
+import { readPost, readPosts } from "../../api/post/read";
 import { authGuard } from "../../utilities/authGuard";
 import { getUserInfo } from "../../utilities/userinfo";
-
+// kan refactore til funk om tiden strekker til. ink actions div.
 authGuard();
 
 const userInfo = getUserInfo();
-console.log("userInfo", userInfo);
-
 const name = userInfo.name;
-const email = userInfo.email;
-console.log(name, email);
 
 document.querySelector(".welcomeUser").innerHTML = name || "Annonymus";
 
+const Allposts = await readPosts();
+console.log(Allposts);
+
+if (Allposts) {
+  Allposts.map((post) => {
+    const article = document.createElement("article");
+
+    const userInfoDiv = document.createElement("div");
+    userInfoDiv.className = "user-Info";
+
+    const articleBody = document.createElement("div");
+    articleBody.className = "article-Body";
+
+    const articleTitle = document.createElement("h2");
+    articleTitle.innerText = post.title;
+    const articleText = document.createElement("p");
+    articleText.innerText = post.body;
+
+    const articleTags = document.createElement("p");
+    articleTags.innerText = post.tags;
+
+    const articleImg = document.createElement("img");
+    articleImg.src = post.media.url;
+    articleImg.alt = post.media.alt || "Post image";
+
+    const articlebtn = document.createElement("button");
+    articlebtn.innerText = "Read Post";
+    articlebtn.addEventListener("click", () => {
+      window.location.href = `/post/single-post/?id=${post.id}`;
+    });
+
+    articleBody.append(
+      articleTitle,
+      articleText,
+      articleTags,
+      articleImg,
+      articlebtn
+    );
+    article.append(userInfoDiv, articleBody);
+    document.querySelector(".posts").append(article);
+  });
+}
