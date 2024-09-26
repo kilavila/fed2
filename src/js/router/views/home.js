7a5189086220015bcfae63e41dff5c64dcfbaff1
kilavila <christer.kilavik@protonmail.com>
2024-09-26 13:04:33 +0200

    adding function on home.js

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,17 +1,21 @@
-import { readPost, readPosts } from "../../api/post/read";
+import { readPosts } from "../../api/post/read";
 import { authGuard } from "../../utilities/authGuard";
 import { getUserInfo } from "../../utilities/userInfo";
 import createPostCards from "../../utilities/post-card";
 
 authGuard();
 
-const userInfo = getUserInfo();
-const name = userInfo.name;
+async function home() {
+  const userInfo = getUserInfo();
+  const name = userInfo.name;
 
-document.querySelector(".welcomeUser").innerHTML = name || "Annonymus";
+  document.querySelector(".welcomeUser").innerHTML = name || "Annonymus";
 
-const Allposts = await readPosts();
+  const Allposts = await readPosts();
 
-const articles = createPostCards(Allposts, false);
+  const articles = createPostCards(Allposts, false);
 
-document.querySelector(".posts").append(articles);
+  document.querySelector(".posts").append(articles);
+}
+
+await home();
