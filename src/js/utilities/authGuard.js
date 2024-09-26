5caa04b57b49b0b7c109e9ad0ed955da3533612d
kilavila <christer.kilavik@protonmail.com>
2024-09-26 13:15:03 +0200

    problems with router or authGuard

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,5 +1,7 @@
 export function authGuard() {
-  if (!localStorage.token) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
   }
